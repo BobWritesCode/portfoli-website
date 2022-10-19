b529d9df0935e2e8f4b0823b3d3b25c434d98675
BobWritesCode <warwick_hart@hotmail.com>
2022-10-19 15:35:34 +0000

    Adjust at what point elements reveal

--- assets/script.js
+++ assets/script.js
@@ -11,9 +11,9 @@ function reveal() {
   for (var i = 0; i < reveals.length; i++) {
     var windowHeight = window.innerHeight;
     var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 500;
+    var elementVisible = windowHeight * 0.80;
 
-    if (elementTop < windowHeight - elementVisible) {
+    if (elementTop < elementVisible) {
       reveals[i].classList.add("active");
     } else {
       reveals[i].classList.remove("active");
