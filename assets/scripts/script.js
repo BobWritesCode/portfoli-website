b8449a473f8edbcdc9ffdd0e3162f99c2ec2a5ce
BobWritesCode <warwick_hart@hotmail.com>
2023-03-06 00:11:38 +0000

    EsLint fixes

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,25 +1,42 @@
-import { certificates } from "./certificates.js";
-import { techSkills } from "./techskills.js";
+/* global $ */
+import { certificates } from './certificates.js';
+import { techSkills } from './techskills.js';
 const certs = certificates;
 const skills = techSkills;
+/**
+ * Open image preview in center of viewpoint.
+ * @param {string} data Received from the DOM
+ */
+function openPreview(data) {
+    $('#image-preview-container').removeClass('d-none').addClass('d-flex');
+    const pic = `./assets/imgs/certificates/${data}`;
+    const img = $('#image-preview-container').find('img');
+    img[0].setAttribute('src', pic);
+}
+/**
+ * Close image preview in center of viewpoint.
+ */
+function closePreview() {
+    $('#image-preview-container').removeClass('d-flex').addClass('d-none');
+}
 function createSkillBars() {
     const container = $('#skill-bars-container');
     skills.forEach(skill => {
-        let newDiv = $(`<div class="animated-progress ${skill.css}"></div>`);
-        newDiv.append(`<span data-progress="${skill.dataProgress}" data-text="${skill.dataText}" aria-hidden="true"></span>`);
+        const newDiv = $(`<div class='animated-progress ${skill.css}'></div>`);
+        newDiv.append(`<span data-progress='${skill.dataProgress}' data-text='${skill.dataText}' aria-hidden='true'></span>`);
         container.append(newDiv);
     });
 }
 function createCertTable() {
     const table = $('#certificate_table');
     certs.forEach(cert => {
-        let newTR = $('<tr class="text-light"></tr>');
+        const newTR = $('<tr class="text-light"></tr>');
         newTR.append(`<td>${cert.icon}</td>`);
-        newTR.append(`<td class="d-none d-md-table-cell">${cert.institute}</td>`);
-        newTR.append(`<td class="orange">${cert.certName}</td>`);
+        newTR.append(`<td class='d-none d-md-table-cell'>${cert.institute}</td>`);
+        newTR.append(`<td class='orange'>${cert.certName}</td>`);
         if (cert.certURL) {
-            let ele = $(`<td class="text-center"><button class="btn-portfolio"><i
-        class="bi bi-file-medical"></i></button></td>`).on('click', function () { openPreview(`${cert.certURL}`); });
+            const ele = $(`<td class='text-center'><button class='btn-portfolio'><i
+        class='bi bi-file-medical'></i></button></td>`).on('click', function () { openPreview(`${cert.certURL}`); });
             newTR.append(ele);
         }
         else {
@@ -36,62 +53,61 @@ function reveal() {
     // Section needs to pass the 20% mark from the bottom of the window to show.
     const elementVisible = window.innerHeight * 0.8;
     // Get top page element.
-    const siteTop = document.getElementById("top").getBoundingClientRect().top;
+    const siteTop = document.getElementById('top').getBoundingClientRect().top;
     // Get bottom page element.
-    const siteBottom = document.getElementById("footer").getBoundingClientRect().bottom;
+    const siteBottom = document.getElementById('footer').getBoundingClientRect().bottom;
     // Check if user at bottom of page.
     if (siteBottom < window.innerHeight + 1) {
         // If user at bottom of page, show all sections
-        $.each($(".reveal"), (_, val) => {
-            val.classList.add("active");
+        $.each($('.reveal'), (_, val) => {
+            val.classList.add('active');
             if (val.getAttribute('id') === 'tech-section') {
-                toggleBars("open");
+                toggleBars('open');
             }
         });
         // Check to see if user at top of page.
     }
     else if (siteTop > 10) {
         // If at top of page hide all but top section
-        $.each($(".reveal"), (_, val) => {
-            val.classList.remove("active");
+        $.each($('.reveal'), (_, val) => {
+            val.classList.remove('active');
             if (val.getAttribute('id') === 'tech-section') {
-                toggleBars("close");
+                toggleBars('close');
             }
-            ;
         });
         // If user not at bottom of page.
     }
     else {
         // As scroll check each element.
-        $.each($(".reveal"), (_, val) => {
+        $.each($('.reveal'), (_, val) => {
             if (val.getBoundingClientRect().top < elementVisible && siteTop !== 0) {
-                val.classList.add("active");
+                val.classList.add('active');
                 if (val.getAttribute('id') === 'tech-section') {
-                    toggleBars("open");
+                    toggleBars('open');
                 }
             }
             else {
-                val.classList.remove("active");
+                val.classList.remove('active');
                 if (val.getAttribute('id') === 'tech-section') {
                     // Tech sections is not currently being shown
-                    toggleBars("close");
+                    toggleBars('close');
                 }
             }
         });
     }
     if (siteTop === 0) {
-        $("#scroll_flash")
-            .find("h3")
+        $('#scroll_flash')
+            .find('h3')
             .delay(2000)
-            .fadeIn("slow", () => {
-            $("#scroll_flash").removeClass("d-none");
+            .fadeIn('slow', () => {
+            $('#scroll_flash').removeClass('d-none');
         });
     }
     else {
-        $("#scroll_flash")
-            .find("h3")
-            .fadeOut("fast", () => {
-            $("#scroll_flash").addClass("d-none");
+        $('#scroll_flash')
+            .find('h3')
+            .fadeOut('fast', () => {
+            $('#scroll_flash').addClass('d-none');
         });
     }
 }
@@ -107,45 +123,27 @@ let inProgress = false;
 let full = false;
 /**
 * Skill progress bar animation.
-* @type {("open" | "close")} If to open progress bars or close.
+* @type {('open' | 'close')} If to open progress bars or close.
 */
 function toggleBars(data) {
-    if (!full && !inProgress && data === "open") {
+    if (!full && !inProgress && data === 'open') {
         // Only queue open animation if bar width at 0
-        if ($(".animated-progress span").first().width() == 0) {
+        if ($('.animated-progress span').first().width() == 0) {
             inProgress = true;
-            $(".animated-progress span").each(function () {
-                $(this).animate({ width: `${$(this).attr("data-progress")}%` }, 5000, function () { full = true; inProgress = false; });
-                const elTxt = $(this).attr("data-text");
-                const txt = typeof elTxt !== "string" ? "" : elTxt;
-                $(this).text(txt).css("text-align", "left");
+            $('.animated-progress span').each(function () {
+                $(this).animate({ width: `${$(this).attr('data-progress')}%` }, 5000, function () { full = true; inProgress = false; });
+                const elTxt = $(this).attr('data-text');
+                const txt = typeof elTxt !== 'string' ? '' : elTxt;
+                $(this).text(txt).css('text-align', 'left');
             });
         }
     }
-    else if (full && !inProgress && data === "close") {
+    else if (full && !inProgress && data === 'close') {
         inProgress = true;
-        $(".animated-progress span").each(function () {
-            $(this).animate({ width: "0%" }, 500, function () { full = false; inProgress = false; });
+        $('.animated-progress span').each(function () {
+            $(this).animate({ width: '0%' }, 500, function () { full = false; inProgress = false; });
         });
     }
-    ;
-}
-;
-/**
- * Open image preview in center of viewpoint.
- * @param {any} data Received from the DOM
- */
-function openPreview(data) {
-    $("#image-preview-container").removeClass("d-none").addClass("d-flex");
-    const pic = `./assets/imgs/certificates/${data}`;
-    const img = $("#image-preview-container").find("img");
-    img[0].setAttribute("src", pic);
-}
-/**
- * Close image preview in center of viewpoint.
- */
-function closePreview() {
-    $("#image-preview-container").removeClass("d-flex").addClass("d-none");
 }
 $(window).on('scroll', function () {
     reveal();
@@ -157,11 +155,10 @@ $(document).ready(function () {
     reveal();
     // Checking to see if tech-section has started open
     // if so, then open progress bars
-    $.each($(".reveal"), (_, val) => {
+    $.each($('.reveal'), (_, val) => {
         if (val.getAttribute('id') === 'tech-section') {
-            toggleBars("open");
+            toggleBars('open');
         }
-        ;
     });
 });
 //# sourceMappingURL=script.js.map
--- assets/scripts/script.ts
+++ assets/scripts/script.ts
@@ -1,43 +1,61 @@
-
+/* global $ */
 import {
   Certificates,
   certificates
-} from "./certificates.js";
+} from './certificates.js';
 
 import {
   TechSkills,
   techSkills
-} from "./techskills.js";
+} from './techskills.js';
 
-const certs: Certificates[] = certificates
-const skills: TechSkills[] = techSkills
+const certs: Certificates[] = certificates;
+const skills: TechSkills[] = techSkills;
+
+/**
+ * Open image preview in center of viewpoint.
+ * @param {string} data Received from the DOM
+ */
+function openPreview(data:string): void {
+  $('#image-preview-container').removeClass('d-none').addClass('d-flex');
+  const pic = `./assets/imgs/certificates/${data}`;
+  const img = $('#image-preview-container').find('img');
+  img[0].setAttribute('src', pic);
+}
+
+/**
+ * Close image preview in center of viewpoint.
+ */
+function closePreview(): void {
+  $('#image-preview-container').removeClass('d-flex').addClass('d-none');
+}
 
 function createSkillBars() {
   const container = $('#skill-bars-container');
   skills.forEach(skill => {
-  let newDiv = $(`<div class="animated-progress ${skill.css}"></div>`)
-  newDiv.append(`<span data-progress="${skill.dataProgress}" data-text="${skill.dataText}" aria-hidden="true"></span>`)
-  container.append(newDiv)
-  })
+  const newDiv = $(`<div class='animated-progress ${skill.css}'></div>`);
+  newDiv.append(`<span data-progress='${skill.dataProgress}' data-text='${skill.dataText}' aria-hidden='true'></span>`);
+  container.append(newDiv);
+  });
 }
 
 function createCertTable () {
-  const table = $('#certificate_table')
+  const table = $('#certificate_table');
   certs.forEach( cert => {
-    let newTR = $('<tr class="text-light"></tr>')
-    newTR.append(`<td>${cert.icon}</td>`)
-    newTR.append(`<td class="d-none d-md-table-cell">${cert.institute}</td>`)
-    newTR.append(`<td class="orange">${cert.certName}</td>`)
+    const newTR = $('<tr class="text-light"></tr>');
+    newTR.append(`<td>${cert.icon}</td>`);
+    newTR.append(`<td class='d-none d-md-table-cell'>${cert.institute}</td>`);
+    newTR.append(`<td class='orange'>${cert.certName}</td>`);
     if (cert.certURL) {
-      let ele = $(`<td class="text-center"><button class="btn-portfolio"><i
-        class="bi bi-file-medical"></i></button></td>`).on('click', function() {openPreview(`${cert.certURL}`)})
-      newTR.append(ele)
+      const ele = $(`<td class='text-center'><button class='btn-portfolio'><i
+        class='bi bi-file-medical'></i></button></td>`).on('click', function() {openPreview(`${cert.certURL}`);});
+      newTR.append(ele);
     } else {
-      newTR.append(`<td></td>`)
+      newTR.append(`<td></td>`);
     }
-    table.find('tbody').append(newTR)
-  })
-  table.addClass('table table-hover table-dark mt-3')
+    table.find('tbody').append(newTR);
+  });
+  table.addClass('table table-hover table-dark mt-3');
 }
 
 /**
@@ -48,63 +66,63 @@ function reveal(): void {
   const elementVisible: number = window.innerHeight * 0.8;
   // Get top page element.
   const siteTop: number = (
-    document.getElementById("top") as HTMLElement
+    document.getElementById('top') as HTMLElement
   ).getBoundingClientRect().top;
   // Get bottom page element.
   const siteBottom: number = (
-    document.getElementById("footer") as HTMLElement
+    document.getElementById('footer') as HTMLElement
   ).getBoundingClientRect().bottom;
 
   // Check if user at bottom of page.
   if (siteBottom < window.innerHeight + 1) {
     // If user at bottom of page, show all sections
-    $.each($(".reveal"), (_, val: HTMLElement): void => {
-      val.classList.add("active");
+    $.each($('.reveal'), (_, val: HTMLElement): void => {
+      val.classList.add('active');
       if (val.getAttribute('id') === 'tech-section') {
-        toggleBars("open");
+        toggleBars('open');
       }
     });
     // Check to see if user at top of page.
   } else if (siteTop > 10) {
     // If at top of page hide all but top section
-    $.each($(".reveal"), (_, val: HTMLElement): void  => {
+    $.each($('.reveal'), (_, val: HTMLElement): void  => {
 
-      val.classList.remove("active");
+      val.classList.remove('active');
       if (val.getAttribute('id') === 'tech-section') {
-        toggleBars("close");
-      };
+        toggleBars('close');
+      }
     });
     // If user not at bottom of page.
   } else {
     // As scroll check each element.
-    $.each($(".reveal"), (_, val: HTMLElement): void  => {
+    $.each($('.reveal'), (_, val: HTMLElement): void  => {
       if (val.getBoundingClientRect().top < elementVisible && siteTop !== 0) {
-        val.classList.add("active");
+        val.classList.add('active');
         if (val.getAttribute('id') === 'tech-section') {
-          toggleBars("open");
+          toggleBars('open');
         }
       } else {
-        val.classList.remove("active");
+        val.classList.remove('active');
         if (val.getAttribute('id') === 'tech-section') {
           // Tech sections is not currently being shown
-          toggleBars("close");
+          toggleBars('close');
         }
       }
     });
   }
 
   if (siteTop === 0) {
-    $("#scroll_flash")
-      .find("h3")
+    $('#scroll_flash')
+      .find('h3')
       .delay(2000)
-      .fadeIn("slow", () => {
-        $("#scroll_flash").removeClass("d-none");
+      .fadeIn('slow', () => {
+        $('#scroll_flash').removeClass('d-none');
       });
   } else {
-    $("#scroll_flash")
-      .find("h3")
-      .fadeOut("fast", () => {
-        $("#scroll_flash").addClass("d-none");
+    $('#scroll_flash')
+      .find('h3')
+      .fadeOut('fast', () => {
+        $('#scroll_flash').addClass('d-none');
       });
   }
 }
@@ -113,54 +131,36 @@ function reveal(): void {
 * Is progress bar animation currently happening.
 * @type {boolean}
 */
-let inProgress: boolean = false
+let inProgress = false;
 
 /**
 * Is progress bar full.
 * @type {boolean}
 */
-let full: boolean = false
+let full = false;
 
 /**
 * Skill progress bar animation.
-* @type {("open" | "close")} If to open progress bars or close.
+* @type {('open' | 'close')} If to open progress bars or close.
 */
-function toggleBars (data: ("open" | "close")): void {
-  if (!full && !inProgress && data === "open") {
+function toggleBars (data: ('open' | 'close')): void {
+  if (!full && !inProgress && data === 'open') {
     // Only queue open animation if bar width at 0
-    if ($(".animated-progress span").first().width() == 0) {
+    if ($('.animated-progress span').first().width() == 0) {
       inProgress = true;
-      $(".animated-progress span").each(function (): void {
-        $(this).animate({ width: `${$(this).attr("data-progress")}%` }, 5000, function(){full=true; inProgress = false;});
-        const elTxt = $(this).attr("data-text");
-        const txt: string = typeof elTxt !== "string" ? "" : elTxt;
-        $(this).text(txt).css("text-align", "left");
+      $('.animated-progress span').each(function (): void {
+        $(this).animate({ width: `${$(this).attr('data-progress')}%` }, 5000, function(){full=true; inProgress = false;});
+        const elTxt = $(this).attr('data-text');
+        const txt: string = typeof elTxt !== 'string' ? '' : elTxt;
+        $(this).text(txt).css('text-align', 'left');
       });
     }
-  } else if (full && !inProgress && data === "close") {
+  } else if (full && !inProgress && data === 'close') {
       inProgress = true;
-      $(".animated-progress span").each(function (): void {
-        $(this).animate({ width: "0%" }, 500, function(){full=false; inProgress = false;});
+      $('.animated-progress span').each(function (): void {
+        $(this).animate({ width: '0%' }, 500, function(){full=false; inProgress = false;});
       });
-  };
-};
-
-/**
- * Open image preview in center of viewpoint.
- * @param {any} data Received from the DOM
- */
-function openPreview(data: any): void {
-  $("#image-preview-container").removeClass("d-none").addClass("d-flex");
-  const pic = `./assets/imgs/certificates/${data}`;
-  const img = $("#image-preview-container").find("img");
-  img[0].setAttribute("src", pic);
-}
-
-/**
- * Close image preview in center of viewpoint.
- */
-function closePreview(): void {
-  $("#image-preview-container").removeClass("d-flex").addClass("d-none");
+  }
 }
 
 $(window).on('scroll', function() {
@@ -168,15 +168,15 @@ $(window).on('scroll', function() {
 });
 
 $(document).ready( function() {
-  $('#btnClosePreview').on('click', function(){closePreview()})
+  $('#btnClosePreview').on('click', function(){closePreview();});
   createCertTable();
   createSkillBars();
   reveal();
   // Checking to see if tech-section has started open
   // if so, then open progress bars
-  $.each($(".reveal"), (_, val: HTMLElement): void => {
+  $.each($('.reveal'), (_, val: HTMLElement): void => {
     if (val.getAttribute('id') === 'tech-section') {
-        toggleBars("open");
-    };
+        toggleBars('open');
+    }
   });
 });
